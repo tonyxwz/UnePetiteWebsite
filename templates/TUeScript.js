97e8c3b06499fd06fd759f6a4ca4a152410ea37f
Tony Xing <tonyxwz@yahoo.com>
2018-07-11 09:58:13 +0200

    Good mobile menu with responsive; color scheme needs calibrating

--- templates/TUeScript.js
+++ templates/TUeScript.js
@@ -2,20 +2,24 @@
 <script>*/
 $(document).ready(function(){
     $('#mobile-btn').click(function(){
-        var display = $('#mobile-menu').css('display');
+        var display = $('.mobile-menu-list').css('display');
         if(display == "none"){
-            $('#mobile-menu').css('display', 'flex');
+            $('.mobile-menu-list').css('display', 'flex');
+            $('.normal-menu').css('border-radius', '5px 5px 0px 0px');
         }else{
-            $('#mobile-menu').css('display', 'none')
+            $('.mobile-menu-list').css('display', 'none');
+            $('.normal-menu').css('border-radius', '5px 5px 5px 5px');
         }
     });
-    var showul = false;
-    $(document).ready(function(){
-        $('li.sub').click(
-        function(){
-            $(this).find('ul.child').toggle();
-        });
+
+    $(window).resize(function(){
+        $('.mobile-menu-list').css('display', 'none');
     });
+    
+    $('li.sub').click(function(){
+        $(this).find('ul.child').toggle();
+    });
+
 })
 /*</script>
 </html>*/
