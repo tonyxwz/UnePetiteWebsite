f9d265033b9bb19402da652a1f3188d080f1122a
Xing <w.xing@student.tue.nl>
2018-07-17 21:12:20 +0200

    Nav bar that works on igem server. Mobile page styles need more work.

--- templates/TUeScript.js
+++ templates/TUeScript.js
@@ -14,12 +14,24 @@ $(document).ready(function(){
 
     $(window).resize(function(){
         $('.mobile-menu-list').css('display', 'none');
+        $('.normal-menu').css('border-radius', '5px 5px 5px 5px');
     });
     
     $('li.sub').click(function(){
         $(this).find('ul.child').toggle();
     });
+    if (bGenerateCat)
+    {
+        catalogueGen();
+    }
+});
 
-})
+function catalogueGen(){
+    var h2s = document.querySelectorAll("div#docContent h2");
+    htmlList = [];
+    for(var i = 0; i<h2s.length;i++){
+        console.log(h2s[i].id);
+    }
+}
 /*</script>
 </html>*/
