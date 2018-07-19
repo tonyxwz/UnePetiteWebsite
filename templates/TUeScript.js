4fece34e274c0d4b0f186574bfda7498d509bbc2
Xing <w.xing@student.tue.nl>
2018-07-19 20:27:48 +0200

    Good toc generator ============= NavMenu page contains a lot of test text

--- templates/TUeScript.js
+++ templates/TUeScript.js
@@ -15,6 +15,7 @@ $(document).ready(function(){
     $(window).resize(function(){
         $('.mobile-menu-list').css('display', 'none');
         $('.normal-menu').css('border-radius', '5px 5px 5px 5px');
+        setTOCWidth();
     });
     
     $('li.sub').click(function(){
@@ -28,11 +29,47 @@ $(document).ready(function(){
 });
 
 function catalogueGen(){
-    var h2s = document.querySelectorAll("div#docContent h2");
-    htmlList = [];
-    for(var i = 0; i<h2s.length;i++){
-        console.log(h2s[i].id);
+    var sections = document.querySelectorAll("div#docContent div.sect");
+    htmlList = ["<ul>\n"];
+    for(var i =0;i<sections.length;i++){
+        sect = sections[i];
+
+        // select h2 within section
+        var h2 = sect.querySelector('h2');
+        h2txt = h2.innerHTML;
+        h2id = idFromTxt(h2txt,h2);
+        htmlList.push("<li>\n");
+        htmlList.push("<a href=#"+h2id+">"+h2txt+"</a>\n");
+
+        // select h3s
+        htmlList.push("<ul>\n");
+        h3s = sect.querySelectorAll('h3');
+        for(var j = 0;j<h3s.length;j++){
+            var h3 = h3s[j];
+            h3txt = h3.innerHTML;
+            h3id = idFromTxt(h3txt,h3);
+            htmlList.push("<li><a href=#"+h3id+">"+h3txt+"</a></li>");
+        }
+        htmlList.push("</ul>\n");
+        htmlList.push("</li>\n");
     }
+    htmlList.push("</ul>\n");
+    
+    // set width of toc
+    setTOCWidth();
+    $("#docJump").html(htmlList.join(""));
+    $('#docJump').toggle();
+}
+
+function setTOCWidth(){
+    var divtoc = document.querySelector("#docJump");
+    divtoc.style.width = divtoc.parentNode.clientWidth - 10;
+}
+
+function idFromTxt(txt,el){
+    var elid = txt.replace(/\s+/g, '');
+    el.id = elid;
+    return elid;
 }
 /*</script>
 </html>*/
